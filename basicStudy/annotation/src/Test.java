ba59035ba87924faa5c56416ccc3eac6195e4125
lucky_Chen <1627784633@qq.com>
2023-04-03 20:02:53 +0800

    提交代码

---
+++ basicStudy/annotation/src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @description://TODO
+ * @author: Luck_chen
+ * @date: 2022/12/18 17:31
+ * @Version 1.0.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        boolean a[] = new boolean[3];
+        a[0] = false;
+        for (boolean b : a) {
+            System.out.println(b);
+        }
+
+    }
+}
